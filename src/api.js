ee0f82517a5d3075f7a55f713796fe3a2f4977d5
Upendar Yadav <upendarr9696@gmail.com.com>
2024-12-28 11:23:03 +0530

    api

--- src/api.js
+++ src/api.js
@@ -1,5 +1,5 @@
-//export const API_URL = "https://xpressfoods-backend.onrender.com"
+export const API_URL = "https://xpressfoods-backend.onrender.com"
 
- export const API_URL = "http://localhost:8000"
+ //export const API_URL = "http://localhost:8000"
  
  export const API_KEY = '777f189a2a984c2e8dfc51abe416f693'; 
